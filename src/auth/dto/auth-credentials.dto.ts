5913118b91de2c2ffdb63dce4fa556a4581f2703
Fahad Akmal <fahad@risingtech.com>
2022-07-05 16:32:45 +0500

    SHUB-43 User login backend T

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,10 +1,14 @@
-import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import {
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+  IsEmail,
+} from 'class-validator';
 
 export class AuthCredentialsDto {
-  @IsString()
-  @MinLength(4)
-  @MaxLength(20)
-  username: string;
+  @IsEmail()
+  email: string;
 
   @IsString()
   @MinLength(8)
