72e281f96e3b03bc4d4e4c3052505f58b28ed2dd
Fahad Akmal <fahad@risingtech.com>
2022-07-20 17:27:43 +0500

    SHUB-90 api to get doumennt types

--- src/common/modules/documentType/documentType.service.ts
+++ src/common/modules/documentType/documentType.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DocumentType } from './entities/document_type.entity';
 
 @Injectable()
-export class DocumentTypeService {}
+export class DocumentTypeService {
+  constructor(
+    @InjectRepository(DocumentType)
+    private decumentTypeRepositery: Repository<DocumentType>,
+  ) {}
+  async getDocumentTypes(): Promise<DocumentType[]> {
+    try {
+      const documentTypes = await this.decumentTypeRepositery.find({
+        where: { isActive: true },
+      });
+      return documentTypes;
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
