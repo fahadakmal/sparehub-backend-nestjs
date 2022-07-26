c195b39773ca44b7ae1f4d16238ffe3d4b4b7559
Fahad Akmal <fahad@risingtech.com>
2022-07-26 15:14:22 +0500

    SHUB-107 api to upload documents

---
+++ src/common/modules/fileUpload/file-upload.service.ts
@@ -0,0 +1,46 @@
+import { S3 } from 'aws-sdk';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class FileUploadService {
+  constructor(private configService: ConfigService) {}
+  async upload(file) {
+    try {
+      const { originalname, mimeType } = file;
+      const acceptMimeTypes = ['image/png', 'image/jpeg'];
+      if (acceptMimeTypes.includes(mimeType)) {
+        throw new BadRequestException();
+      }
+      const bucketS3 = this.configService.get('S3_BUCKET_NAME');
+      return await this.uploadS3(file.buffer, bucketS3, originalname);
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+  }
+
+  async uploadS3(file, bucket, name) {
+    const s3 = this.getS3();
+    const params = {
+      Bucket: bucket,
+      Key: String(name),
+      Body: file,
+    };
+    return new Promise((resolve, reject) => {
+      s3.upload(params, (err, data) => {
+        if (err) {
+          reject(err.message);
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  getS3() {
+    return new S3({
+      accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      region: this.configService.get('AWS_S3_REGION'),
+    });
+  }
+}
