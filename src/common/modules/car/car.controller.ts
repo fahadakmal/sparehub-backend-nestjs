611f001eab850acb1c94dab33948f806d131edd4
Fahad Akmal <fahad@risingtech.com>
2022-08-18 18:03:36 +0500

    SHUB-170 api to get model for a particular make

--- src/common/modules/car/car.controller.ts
+++ src/common/modules/car/car.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { AwsCognitoGuard } from 'src/auth/guards/awsCognito.guard';
 import { Serialize } from 'src/common/interceptors/serialize.intercepter';
 import { CarService } from './car.service';
 import { CarMakeDto } from './dtos/car_make.dto';
+import { CarModelDto } from './dtos/car_model.dto';
 import { CarMake } from './entities/car_make.entity';
+import { CarModel } from './entities/car_model.entity';
 
 @UseGuards(AwsCognitoGuard)
 @Controller('car')
@@ -14,5 +16,10 @@ export class CarController {
   @Get('/makers')
   getCarMakers(): Promise<CarMake[]> {
     return this.carService.getCarMakers();
+  }
+  @Serialize(CarModelDto)
+  @Get('/maker/:makerId/models')
+  getMakerModels(@Param('makerId') makerId: string): Promise<CarModel[]> {
+    return this.carService.getMakerModels(makerId);
   }
 }
