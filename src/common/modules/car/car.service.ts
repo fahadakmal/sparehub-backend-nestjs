69d66347bd1cd4d89f409f2109d17f2d95ecab85
Fahad Akmal <fahad@risingtech.com>
2022-08-18 18:03:36 +0500

    SHUB-170 api to get model for a particular make

--- src/common/modules/car/car.service.ts
+++ src/common/modules/car/car.service.ts
@@ -6,12 +6,15 @@ import {
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CarMake } from './entities/car_make.entity';
+import { CarModel } from './entities/car_model.entity';
 
 @Injectable()
 export class CarService {
   constructor(
     @InjectRepository(CarMake)
     private carMakeRepositery: Repository<CarMake>,
+    @InjectRepository(CarModel)
+    private carModelRepositery: Repository<CarModel>,
   ) {}
 
   async getCarMakers(): Promise<CarMake[]> {
@@ -21,6 +24,21 @@ export class CarService {
         throw new NotFoundException();
       }
       return carMakers;
+    } catch (error) {
+      throw new BadRequestException();
+    }
+  }
+
+  async getMakerModels(makerId: string): Promise<CarModel[]> {
+    try {
+      const makerModels = await this.carModelRepositery.findBy({
+        isActive: true,
+        make: { id: parseInt(makerId) },
+      });
+      if (!makerModels) {
+        throw new NotFoundException();
+      }
+      return makerModels;
     } catch (error) {
       throw new BadRequestException();
     }
