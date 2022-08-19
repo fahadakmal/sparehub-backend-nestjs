39c46a8ff82cda85691c987ee473f0fdaac0bda4
Fahad Akmal <fahad@risingtech.com>
2022-08-19 16:04:33 +0500

    SHUB-176 seller store locations

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -6,15 +6,17 @@ import {
   Get,
   UseInterceptors,
   ClassSerializerInterceptor,
+  Param,
 } from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator/get_user.decorator';
 import { AwsCognitoGuard } from 'src/auth/guards/awsCognito.guard';
 import { User } from 'src/auth/entities/user.entity';
 import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dtos/create_company.dto';
+import { CompanyStore } from './entities/company_store.entity';
 
 @Controller('company')
-@UseGuards(AwsCognitoGuard)
+// @UseGuards(AwsCognitoGuard)
 export class CompanyController {
   constructor(private companyService: CompanyService) {}
 
@@ -30,5 +32,12 @@ export class CompanyController {
   @Get()
   getCompany(@GetUser() user: User) {
     return this.companyService.getCompany(user);
+  }
+
+  @Get('/:companyId/stores')
+  getCompanyStores(
+    @Param('companyId') companyId: string,
+  ): Promise<CompanyStore[]> {
+    return this.companyService.getCompanyStores(companyId);
   }
 }
