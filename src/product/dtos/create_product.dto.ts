3a45cf0cf00d5e70d3eb68a3e31910be53bf4de8
Fahad Akmal <fahad@risingtech.com>
2022-08-25 18:35:04 +0500

    SHUB-160 api to save product

--- src/product/dtos/create_product.dto.ts
+++ src/product/dtos/create_product.dto.ts
@@ -1,23 +1,67 @@
 import { Type } from 'class-transformer';
-import { IsOptional, ValidateNested } from 'class-validator';
-import { CreateProductBasicDetailDto } from './create_product_basic.dto';
+import {
+  ValidateNested,
+  IsArray,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { CreateProductFitmentDto } from './create_product_fitment.dto';
 import { CreateProductInventoryDto } from './create_product_inventory.dto';
 import { CreateProductMediaDto } from './create_product_media.dto';
+import { ProductStatus } from 'src/common/constants/enums/product_status.enum';
+import { ProductCategory } from 'src/common/modules/product_category/entities/product_category.entity';
+import { Company } from 'src/company/entities/company.entity';
 
 export class CreateProductDto {
-  @ValidateNested()
-  @Type(() => CreateProductBasicDetailDto)
-  productBasicDetail: CreateProductBasicDetailDto;
   @IsOptional()
+  id: number;
+
+  @IsString()
+  productName: string;
+
+  @IsString()
+  productNameAr: string;
+
+  @IsString()
+  itemCode: string;
+
+  @IsString()
+  description: string;
+
+  @IsString()
+  descriptionAr: string;
+
+  @IsNumber()
+  salePrice: number;
+
+  @IsNumber()
+  salePriceNet: number;
+
+  @IsNumber()
+  discount: number;
+
+  @IsEnum(ProductStatus)
+  status: ProductStatus;
+
+  @IsString()
+  sku: string;
+
+  @IsString()
+  barcode: string;
+
+  @IsNumber()
+  company: Company;
+
+  @IsArray()
+  categories: ProductCategory[];
   @ValidateNested({ each: true })
   @Type(() => CreateProductInventoryDto)
   inventory: CreateProductInventoryDto[];
-  @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => CreateProductFitmentDto)
   fitments: CreateProductFitmentDto[];
-  @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => CreateProductMediaDto)
   mediaFiles: CreateProductMediaDto[];
